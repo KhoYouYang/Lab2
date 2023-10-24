0e40f7ce325910b965acdabec8633a871fe4708b
YouYang <YOUYANG.22@ichat.sp.edu.sg>
2023-10-24 17:46:06 +0800

    Labelling of answers

--- Lab2.py
+++ Lab2.py
@@ -4,11 +4,15 @@ def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     numlist = get_user_input()
-    print(numlist)
-    calc_average(numlist)
-    find_min_max(numlist)
-    sort_temperature(numlist)
-    calc_median_temperature(numlist)
+    print("List of numbers: " + str(numlist))
+    average = calc_average(numlist)
+    print("Average value: " + str(average))
+    minmax = find_min_max(numlist)
+    print(minmax)
+    tempsort = sort_temperature(numlist)
+    print("Sorted values: " + str(tempsort))
+    mediantemp = calc_median_temperature(numlist)
+    print("Median Value: " + str(mediantemp))
 
 def display_main_menu():
     print("Enter some numbers separated by commas (e.g. 5, 67, 32)")
@@ -17,7 +21,6 @@ def calc_average(numlist):
     for x in numlist:
         total += x
     average = total/len(numlist)
-    print(average)
     return average
 
 def get_user_input():
@@ -26,14 +29,14 @@ def get_user_input():
     floatlist = [float(x) for x in splitlist]
     return floatlist
 def find_min_max(numlist):
-    print(min(numlist))
-    print(max(numlist))
+    x = ["Minimum value = " + str(min(numlist)), "Maximum value = " + str(max(numlist))]
+    return x
 def sort_temperature(numlist):
     numlist.sort()
-    print(numlist)
+    return numlist
 def calc_median_temperature(numlist):
     median = statistics.median(numlist)
-    print(median)
+    return median
 
 if __name__ == "__main__":
     main()
